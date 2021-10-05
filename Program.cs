76cee1246e16f19baf6e765d478cbd147864b020
Samson Amaugo <sammychinedu2ky@gmail.com>
2021-10-05 06:44:38 +0100

    reduction via linq

--- Program.cs
+++ Program.cs
@@ -6,46 +6,41 @@ namespace SHANON_FANO
 {
     class Program
     {
-        static public IDictionary<char, int> ReturnWordCount(string word)
+        static  IDictionary<char, int> ReturnWordCount(string word)
         {
             Dictionary<char, int> obj = new();
-             word.ToList().ForEach(i =>
-            {
-                var found = obj.TryGetValue(i, out int value) ? obj[i]++ : obj[i] = 1;
-            });
+            word.ToList().ForEach(i =>
+           {
+               var found = obj.TryGetValue(i, out int value) ? obj[i]++ : obj[i] = 1;
+           });
             Dictionary<char, int> sorted = obj.OrderByDescending(i => i.Value).ToDictionary(i => i.Key, i => i.Value);
             return sorted;
         }
-
-        static public IDictionary<char, string> CreateTree(string word)
+        static Dictionary<char, string> answer = new();
+        static  IDictionary<char, string> CreateTree(string word)
         {
             IDictionary<char, int> probClass = ReturnWordCount(word);
-            Dictionary<char, string> answer = new();
-            probClass.Keys.ToList().ForEach(i =>
-            {
-                answer[i] = "";
-            });
+            probClass.Keys.ToList().ForEach(i => { answer[i] = ""; });
             if (probClass.Keys.Count > 1)
             {
-                RecursiveMethod(group: probClass, answer: answer);
+                RecursiveMethod(group: probClass);
             }
             else answer[probClass.Keys.ToList()[0]] = "0";
             return answer;
         }
 
-        static public void RecursiveMethod(
-            IDictionary<char, string> answer = null,
+        static  void RecursiveMethod(
             IDictionary<char, int> group = null,
             string accumulation = ""
            )
         {
-            group.Keys.ToList().ForEach(i=>answer[i]=accumulation);
+            group.Keys.ToList().ForEach(i => answer[i] = accumulation);
             List<char> keys = group.Keys.ToList();
             List<int> val = group.Values.ToList();
             List<int> diff = new();
             if (val.Count > 1)
             {
-                diff = val.Select((_, index) => Math.Abs(val.ToArray()[0 ..(index + 1)].Sum() - val.ToArray()[(index + 1)..].Sum())).ToList().GetRange(0, val.Count - 1);
+                diff = val.Select((_, index) => Math.Abs(val.ToArray()[0..(index + 1)].Sum() - val.ToArray()[(index + 1)..].Sum())).ToList().GetRange(0, val.Count - 1);
                 var indexOfMin = diff.IndexOf(diff.Min());
                 Dictionary<char, int> leftParameter = new();
                 Dictionary<char, int> rightParameter = new();
@@ -59,11 +54,11 @@ namespace SHANON_FANO
                 }
                 if (leftParameter.Values.Count >= 0)
                 {
-                    RecursiveMethod(answer: answer, group: leftParameter, accumulation: accumulation + "0");
+                    RecursiveMethod(group: leftParameter, accumulation: accumulation + "0");
                 }
                 if (rightParameter.Values.Count >= 0)
                 {
-                    RecursiveMethod(answer: answer, group: rightParameter, accumulation: accumulation + "1");
+                    RecursiveMethod(group: rightParameter, accumulation: accumulation + "1");
                 }
             }
 
